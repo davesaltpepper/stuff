8cbaef6b3e923093c1643c6a93adb7f4cd2585f4
dave muir <david@addsaltandpepper.com>
2012-07-05 13:19:44 -0400

    added wordpressdummy folder

---
+++ wp-config_local.php
@@ -0,0 +1,14 @@
+<?php
+
+	
+	/** The name of the database for WordPress */
+	define('DB_NAME', "wordpressdummybase");
+	
+	/** MySQL database username */
+	define('DB_USER', "root");
+	
+	/** MySQL database password */
+	define('DB_PASSWORD', "root");
+	
+	/** MySQL hostname */
+	define('DB_HOST', "localhost");
